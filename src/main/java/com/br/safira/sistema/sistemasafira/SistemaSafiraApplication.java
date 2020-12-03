8940b062b4f3dadcd96a189482ac0e343ddc79c9
André Ulle <andrerm@ciandt.com>
2020-12-03 09:23:54 -0300

    [Setup Inicial] Configuração inicial do SpringBoot

---
+++ src/main/java/com/br/safira/sistema/sistemasafira/SistemaSafiraApplication.java
@@ -0,0 +1,13 @@
+package com.br.safira.sistema.sistemasafira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSafiraApplication {
+
+	public static void main(String[] args) {
+				SpringApplication.run(SistemaSafiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/safira/sistema/sistemasafira/SistemaSafiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.safira.sistema.sistemasafira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSafiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
