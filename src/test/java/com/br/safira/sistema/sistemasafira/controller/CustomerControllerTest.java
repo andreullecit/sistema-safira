eb0dd8e21f974a4397a78a5d476da47d4fbb9fdc
Fabiano Luiz Vieira da Silva <CIANDT\fabianolv@lnb021895bhz.ciandt.global>
2020-12-16 13:53:53 -0300

    [Setup Inicial] Testes unitarios controller

--- src/test/java/com/br/safira/sistema/sistemasafira/controller/CustomerControllerTest.java
+++ src/test/java/com/br/safira/sistema/sistemasafira/controller/CustomerControllerTest.java
@@ -1,2 +1,54 @@
-package com.br.safira.sistema.sistemasafira.controller;public class CustomerControllerTest {
+package com.br.safira.sistema.sistemasafira.controller;
+
+import com.br.safira.sistema.sistemasafira.service.CustomerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerControllerTest {
+
+    private String idTest = "1";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerService customerService;
+
+    @Test
+    public void shouldReturnNotFoundForDelete() throws Exception {
+        when(customerService.deleteCustomer(this.idTest)).thenReturn(false);
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .delete("/customer/{id}", idTest)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void shouldReturnOkForDelete() throws Exception {
+        when(customerService.deleteCustomer(idTest)).thenReturn(true);
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .delete("/customer/{id}", idTest)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
 }
