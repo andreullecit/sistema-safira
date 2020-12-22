a5de4b88fba5e48c8e2aa2cb8498637b116d124a
Fabiano Luiz <fabianolv@ciandt.com>
2020-12-22 16:12:06 -0300

    [BLUO-323][BLUO-350] Adicionando teste unitario para o metodo saveCandidate da classe CandidateService

---
+++ src/test/java/com/br/safira/sistema/sistemasafira/service/CandidateServiceTest.java
@@ -0,0 +1,32 @@
+package com.br.safira.sistema.sistemasafira.service;
+
+import com.br.safira.sistema.sistemasafira.model.Candidate;
+import com.br.safira.sistema.sistemasafira.repository.CandidateRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CandidateServiceTest {
+
+    @Mock
+    CandidateRepository candidateRepository;
+
+    @InjectMocks
+    CandidateService candidateService = new CandidateService();
+
+    @Test
+    public void candidateSaved() {
+        Candidate candidate = Candidate.builder().fullName("Test").build();
+        when(this.candidateRepository.save(Mockito.any())).thenReturn(Mockito.any());
+
+        this.candidateService.saveCandidate(candidate);
+
+        Mockito.verify(this.candidateRepository, Mockito.times(1)).save(candidate);
+    }
+}
