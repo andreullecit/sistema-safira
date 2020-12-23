651813d40c47ee02a864c04ed57b56d7bda7d5e9
Fabio Calcagno <fcalcagno@ciandt.com>
2020-12-23 16:30:17 -0300

    [BLUO-324][BLUO-337] add assinatura no repository findByStatus

--- src/main/java/com/br/safira/sistema/sistemasafira/repository/CandidateRepository.java
+++ src/main/java/com/br/safira/sistema/sistemasafira/repository/CandidateRepository.java
@@ -3,6 +3,8 @@ package com.br.safira.sistema.sistemasafira.repository;
 import com.br.safira.sistema.sistemasafira.model.Candidate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CandidateRepository extends MongoRepository<Candidate, String> {
+import java.util.List;
 
+public interface CandidateRepository extends MongoRepository<Candidate, String> {
+    public List<Candidate> findByStatus(char status);
 }
