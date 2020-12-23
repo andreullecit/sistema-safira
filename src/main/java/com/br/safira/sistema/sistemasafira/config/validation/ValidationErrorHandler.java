b46aa114aaa20e95681ca1f99850e47a5ab365ae
Fabio Calcagno <fcalcagno@ciandt.com>
2020-12-23 10:57:19 -0300

    validação de erros e properties adicionados

--- src/main/java/com/br/safira/sistema/sistemasafira/config/validation/ValidationErrorHandler.java
+++ src/main/java/com/br/safira/sistema/sistemasafira/config/validation/ValidationErrorHandler.java
@@ -1,8 +1,10 @@
 package com.br.safira.sistema.sistemasafira.config.validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,29 +12,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
+@Configuration
+@PropertySource("classpath:application.properties")
 public class ValidationErrorHandler {
 
     @Autowired
     private MessageSource messageSource;
 
+    @Value("${VALIDATION_REQUIRED_FIELDS}")
+    private static String validationMessage;
+
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<ValidationErrorDto> handle(MethodArgumentNotValidException exception) {
-        List<ValidationErrorDto> dto = new ArrayList<>();
+    public String handle(MethodArgumentNotValidException exception) {
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
-
-        fieldErrors.forEach(e -> {
-            String message = messageSource.getMessage(e, LocaleContextHolder.getLocale());
-            ValidationErrorDto error = new ValidationErrorDto(message, e.getField());
-
-            dto.add(error);
+        StringBuilder message = new StringBuilder();
+        message.append(validationMessage);
+        fieldErrors.forEach(e-> {
+            message.append(e.getField()).append(", ");
         });
-
-        return dto;
+        return message.toString();
     }
 
 }
